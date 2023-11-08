276bab1b792390b42846800c6c1cff08e0ff22d1
ah00ee <nhoahyoung@gmail.com>
2023-11-08 19:20:58 +0900

    fix: Champion 모델 생성자 수정

--- league/models.py
+++ league/models.py
@@ -78,11 +78,11 @@ class ChampionMastery:
 
 class Champion:
     def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
         self.id = kwargs["key"]
-        self.name = kwargs["id"]
-        self.stats = kwargs["stats"]
+        self.key = kwargs["id"]
 
     def __str__(self) -> str:
-        return self.id + " " + self.name
+        return self.id + " " + self.key
 
         
