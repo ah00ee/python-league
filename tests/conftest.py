3726440a5ddbf661360032c8bb2a1576e15a6b2c
ah00ee <nhoahyoung@gmail.com>
2023-11-10 14:33:09 +0900

    add: models.py 테스트 코드 추가

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,12 @@ from league import LeagueAPI
 @pytest.fixture()
 def api():
     api = LeagueAPI(
-                api_key="api_key",
+                api_key="Your API KEY",
                 platform="platform",
                 region="region"
     )
     return api
+
+@pytest.fixture()
+def summoner(api):
+    return api.get_summoner("Colleen")
